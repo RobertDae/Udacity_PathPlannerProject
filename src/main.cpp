56651b53eebed5c81a5ed0609ae5eab77657dcdc
Robert D <robertrd@gmx.de>
2019-09-18 22:10:26 +0200

    correct the factor 0.2 -->0.02 in the N formula! for the distance between points in the spline

--- src/main.cpp
+++ src/main.cpp
@@ -243,11 +243,11 @@ int main() {
       double x_add_on = 0;  //Ok
 
        //fill up the missing points (regarding the last movement step of the car) so that we allwas have num_pts_to_have_in_the_list
-       const int PTS_IN_LST = 50;
+      const int PTS_IN_LST = 50;
 
-      for (int i =0; i <= (PTS_IN_LST - previous_path_x.size()); i++)
+      for (int i =0; i <= 50 - previous_path_x.size(); i++)
       {
-       double  N =(target_dist/(0.2*ref_vel/2.24)); // number of elements of the spline
+       double  N =(target_dist/(0.02*ref_vel/2.24)); // number of elements of the spline
        double  x_point = x_add_on +(target_x)/N;
        double  y_point = s(x_point);
 
